4aae69202ab83fef8f168e96d085991ad78785ff
Dhruv Agarwal <dhruv.agarwal433@gmail.com>
2025-09-02 13:10:41 +0530

    Structure

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import recommendation
+import recommendation as recommendation
 
 app = FastAPI()
 
